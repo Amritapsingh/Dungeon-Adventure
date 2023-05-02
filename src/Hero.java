11c5e9986200e331c8dbd929285593d3da497df4
Amrit Singh <amrit9928@gmail.com>
2023-05-02 12:26:34 -0700

    Added fields to hero class

--- src/Hero.java
+++ src/Hero.java
@@ -1,2 +1,15 @@
 public class Hero {
+    private String name;
+    private int hp;
+    private int damage;
+    private int maxHp;
+    private int maxDamage;
+
+    void Hero_attack() {
+
+    }
+    void Hero_block() {
+
+    }
+
 }
