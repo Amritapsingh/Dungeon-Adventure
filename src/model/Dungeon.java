13addec28743ddf492ba82e547d098d8ed5a2a6a
Akil Turner-Richards <ki1orich@gmail.com>
2023-05-21 17:49:45 -0700

    refactored dungeon and room classes, added stack for traversal, and added functions to obtain unvisited neighbors for a room

--- src/model/Dungeon.java
+++ src/model/Dungeon.java
@@ -1,58 +1,52 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
+import java.util.Stack;
 
 public class Dungeon {
     private int rows;
     private int cols;
     private Room[][] maze;
+    private Stack<Room> roomStack;
+    private int numRooms;
+    private int visitedRooms;
 
     public Dungeon(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
         this.maze = new Room[rows][cols];
+        this.roomStack =  new Stack<>();
+        this.numRooms = rows * cols;
+        this.visitedRooms = 0;
         generateMaze();
     }
 
     private void generateMaze() {
         Random random = new Random();
-
         // Generate rooms
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                maze[i][j] = new Room(i, j);
-                maze[i][j].generateItems();
+                maze[i][j] = new Room(j, i);
             }
         }
-
-        // Generate connections
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                Room room = maze[i][j];
-                if (i > 0) {
-                    Room northRoom = maze[i - 1][j];
-                    room.addDoor("N", northRoom);
-                    northRoom.addDoor("S", room);
-                }
-                if (i < rows - 1) {
-                    Room southRoom = maze[i + 1][j];
-                    room.addDoor("S", southRoom);
-                    southRoom.addDoor("N", room);
-                }
-                if (j > 0) {
-                    Room westRoom = maze[i][j - 1];
-                    room.addDoor("W", westRoom);
-                    westRoom.addDoor("E", room);
-                }
-                if (j < cols - 1) {
-                    Room eastRoom = maze[i][j + 1];
-                    room.addDoor("E", eastRoom);
-                    eastRoom.addDoor("W", room);
-                }
-            }
+        Room room = getRandomRoom();
+        System.out.println();
+        roomStack.push(room);
+        room.setIsVisited(true);
+        visitedRooms++;
+        Room neighbor = getNeighborOf(room);
+        room.display();
+        ArrayList<Room> neighbors = room.getRoomNeighbors();
+        System.out.println();
+        for (int i = 0; i < neighbors.size(); i++) {
+            Room printRoom = neighbors.get(i);
+            printRoom.display();
         }
+        System.out.println();
+//        do {
+//
+//        } while (visitedRooms != numRooms);
     }
 
     public Room getRoom(int x, int y) {
@@ -69,19 +63,47 @@ public class Dungeon {
         return maze[x][y];
     }
 
+    public Room getNeighborOf(Room room) {
+        getAllNeighbors(room);
+        return room;
+    }
+
+    public void getAllNeighbors(Room room) {
+        ArrayList<Room> neighbors = room.getRoomNeighbors();
+        if (room.y > 0 && !maze[room.y - 1][room.x].getIsVisited()) {
+            neighbors.add(maze[room.y - 1][room.x]);
+        }
+
+        // Down
+        if (room.y < rows - 1 && !maze[room.y + 1][room.x].getIsVisited()) {
+            neighbors.add(maze[room.y + 1][room.x]);
+        }
+
+        // Left
+        if (room.x > 0 && !maze[room.y][room.x - 1].getIsVisited()) {
+            neighbors.add(maze[room.y][room.x - 1]);
+        }
+
+        // Right
+        if (room.x < cols - 1 && !maze[room.y][room.x + 1].getIsVisited()) {
+            neighbors.add(maze[room.y][room.x + 1]);
+        }
+    }
     public void printMaze() {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 Room room = maze[i][j];
                 room.display();
             }
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
         int rows = 5;
-        int cols = 5;
+        int cols = 8;
         Dungeon generator = new Dungeon(rows, cols);
+
         generator.printMaze();
     }
 }
--- src/model/Room.java
+++ src/model/Room.java
@@ -6,31 +6,23 @@ import java.util.Map;
 import java.util.Random;
 
 public class Room {
-    boolean myNorthDoor;
-    boolean mySouthDoor;
-    boolean myEastDoor;
-    boolean myWestDoor;
     boolean myIsEnter;
     boolean myIsExit;
-    private List<String> myItems;
-    private Map<String, Boolean> myDoors;
     private boolean myIsVisited;
+    public int x;
+    public int y;
+    private ArrayList<Room> roomNeighbors;
+    public int neighbors;
     private static final String[] DIRECTIONS = {"N", "S", "E", "W"};
 
-    public Room() {
-        Random theRandom = new Random();
+    public Room(int x, int y) {
+        this.x = x;
+        this.y = y;
         myIsVisited = false;
-        myNorthDoor = theRandom.nextBoolean();
-        mySouthDoor = theRandom.nextBoolean();
-        myEastDoor = theRandom.nextBoolean();
-        myWestDoor = theRandom.nextBoolean();
-        myItems = new ArrayList<>();
-        myDoors = Map.of("N", false, "S", false, "E", false, "W", false);
         myIsEnter = false;
         myIsExit = false;
-    }
-
-    public Room(int i, int j) {
+        this.roomNeighbors = new ArrayList<>();
+        neighbors = 0;
     }
 
     public boolean getIsVisited() {
@@ -40,59 +32,13 @@ public class Room {
         myIsVisited = isVisited;
     }
 
-    public void generateItems() {
-        double random = Math.random();
-        if (random < 0.1) {
-            if (random < 0.033) {
-                myItems.add("Healing Potion");
-            }
-            if (random < 0.066) {
-                myItems.add("Vision Potion");
-            }
-            if (random < 0.1) {
-                myItems.add("Pit");
-            }
-        }
-    }
+    public ArrayList<Room> getRoomNeighbors() { return roomNeighbors; }
+
 
     public void display() {
-        StringBuilder graphicalRepr = new StringBuilder();
-        if (!myDoors.get("N")) {
-            graphicalRepr.append("***\n");
-        } else {
-            graphicalRepr.append("*-*\n");
-        }
-        System.out.print(graphicalRepr.toString());
+        //StringBuilder graphicalRepr = new StringBuilder();
+        System.out.print("[" + x + "," + y + "]");
+
     }
 
-    public void addDoor(String direction, Room adjacentRoom) {
-        if (adjacentRoom != null && containsDirection(direction)) {
-            myDoors.put(direction, true);
-            adjacentRoom.myDoors.put(getOppositeDirection(direction), true);
-        }
-    }
-
-    private boolean containsDirection(String direction) {
-        for (String dir : DIRECTIONS) {
-            if (dir.equals(direction)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private String getOppositeDirection(String direction) {
-        switch (direction) {
-            case "N":
-                return "S";
-            case "S":
-                return "N";
-            case "E":
-                return "W";
-            case "W":
-                return "E";
-            default:
-                return "";
-        }
-    }
 }
